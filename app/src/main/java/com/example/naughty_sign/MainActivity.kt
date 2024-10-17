1fb2781bccd21a3369f191c292b497351e4d8a19
____________________ <zelmarhernanramilopiazzoli2202@gmail.com>
2024-10-17 20:43:07 +0200

    add fragments and replace equals activities

--- app/src/main/java/com/example/naughty_sign/MainActivity.kt
+++ app/src/main/java/com/example/naughty_sign/MainActivity.kt
@@ -8,29 +8,29 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cardview.ItemData
 import com.example.cardview.RecyclerViewAdapter
-import com.example.naughty_sign.databinding.LikesBinding
-import com.example.naughty_sign.databinding.MainBinding
-import com.example.naughty_sign.databinding.MatchesBinding
+import com.example.naughty_sign.databinding.ActivityMainBinding
+import com.example.naughty_sign.databinding.FragmentLikesBinding
+import com.example.naughty_sign.databinding.FragmentMatchesBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: MainBinding;
-    lateinit var likesBinding: LikesBinding;
-    lateinit var matchesBinding: MatchesBinding;
+    lateinit var binding: ActivityMainBinding;
+    lateinit var likesBinding: FragmentLikesBinding;
+    lateinit var matchesBinding: FragmentMatchesBinding;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.main)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        binding = MainBinding.inflate(layoutInflater)
-        likesBinding = LikesBinding.inflate(layoutInflater)
-        matchesBinding = MatchesBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        likesBinding = FragmentLikesBinding.inflate(layoutInflater)
+        matchesBinding = FragmentMatchesBinding.inflate(layoutInflater)
 
         binding.recycleView.layoutManager = LinearLayoutManager(this)
         val items = listOf(
