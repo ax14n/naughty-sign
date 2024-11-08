ed4393220951e1c748b83a25b3b85674a22fa6e9
____________________ <zelmarhernanramilopiazzoli2202@gmail.com>
2024-11-08 13:50:09 +0100

    Implement navigation between actual match and like profiles

--- app/src/main/java/com/example/naughty_sign/FragmentMatchProfile.kt
+++ app/src/main/java/com/example/naughty_sign/FragmentMatchProfile.kt
@@ -1,18 +1,21 @@
 package com.example.naughty_sign
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.example.naughty_sign.databinding.FragmentMatchProfileBinding
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.material.chip.Chip
+import kotlinx.coroutines.launch
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val PARAM1 = "userId"
 
 /**
  * A simple [Fragment] subclass.
@@ -21,15 +24,18 @@ private const val ARG_PARAM2 = "param2"
  */
 class FragmentMatchProfile : Fragment(), OnMapReadyCallback {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var param1: Int? = null
+
     private lateinit var binding: FragmentMatchProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /*
+        * Obtengo el parámetro que se lo asignó a este fragmento mediante la clave
+        * */
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            param1 = it.getInt(PARAM1)
         }
     }
 
@@ -41,7 +47,71 @@ class FragmentMatchProfile : Fragment(), OnMapReadyCallback {
         binding = FragmentMatchProfileBinding.inflate(inflater, container, false)
         binding.map.onCreate(savedInstanceState)
         binding.map.getMapAsync(this)
+        loadProfile()
         return binding.root
+    }
+
+    /**
+     * Carga el pefil del usuario tras leer el JSON del servidor.
+     */
+    private fun loadProfile() {
+        /*
+        * Inicia una corutina
+        * */
+        lifecycleScope.launch {
+            try {
+
+                /*
+                * Almaceno la lista de usuarios y, si la respuesta ha sido efectiva, relleno el perfil
+                * con los datos del usuario.
+                * */
+                val response = RetrofitInstance().api.getUsers()
+                if (response.isSuccessful) {
+                    response.body()?.let { users ->
+                        /*
+                        * Recorro los usuarios y relleno su perfil según el ID.
+                        * */
+                        for (user in users) {
+                            if (user.id == param1) {
+                                binding.profileName.text = user.nombre
+                                binding.profileQuote.text = user.cita
+                                binding.profileProfession.text = user.profesion
+                                binding.profileCity.text = user.ciudad
+                                binding.profileDescription.text = user.descripcion
+
+                                /*
+                                * Obtengo los intereses del usuario y los recorro para formar chips
+                                * */
+                                for (interest in user.intereses) {
+                                    val chipInteres = Chip(requireContext())
+                                    chipInteres.text = interest
+                                    chipInteres.isCloseIconVisible = false
+
+                                    // Ajusta los colores para mejorar la estética
+                                    chipInteres.setChipBackgroundColorResource(R.color.seashell) // Fondo más suaveç
+                                    chipInteres.setTextSize(9F)
+                                    chipInteres.setTextColor(R.color.dark_orange.toInt()) // Texto contrastante
+                                    chipInteres.setChipStrokeColorResource(R.color.silver) // Borde sutil
+                                    chipInteres.setChipStrokeWidth(2f) // Grosor del borde
+                                    chipInteres.setPadding(20, 10, 20, 10) // Ajustar el padding
+
+                                    // Desactivar la selección
+                                    chipInteres.isClickable = false
+                                    chipInteres.isCheckable = false
+
+                                    binding.chipGroup.addView(chipInteres)
+
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    Log.e("API ERROR", "ERROR:  ${response.code()} - ${response.message()}")
+                }
+            } catch (e: Exception) {
+                Log.e("NETWORK ERROR", "Exception: $e")
+            }
+        }
     }
 
     companion object {
@@ -50,20 +120,17 @@ class FragmentMatchProfile : Fragment(), OnMapReadyCallback {
          * this fragment using the provided parameters.
          *
          * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment FragmentMatchProfile.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(param1: Int) =
             FragmentMatchProfile().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putInt(PARAM1.toString(), param1)
                 }
             }
     }
-
 
 
     override fun onMapReady(p0: GoogleMap) {
--- app/src/main/java/com/example/naughty_sign/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/naughty_sign/RecyclerViewAdapter.kt
@@ -1,33 +1,42 @@
-    package com.example.cardview
+package com.example.cardview
 
-    import android.view.LayoutInflater
-    import android.view.ViewGroup
-    import android.widget.Toast
-    import androidx.recyclerview.widget.RecyclerView
-    import com.example.naughty_sign.databinding.ItemCardViewBinding
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.example.naughty_sign.FragmentMatchProfile
+import com.example.naughty_sign.R
+import com.example.naughty_sign.databinding.ItemCardViewBinding
 
-    class RecyclerViewAdapter(private val items: List<ItemData>) :
-        RecyclerView.Adapter<ItemViewHolder>() {
+class RecyclerViewAdapter(private val items: List<ItemData>) :
+    RecyclerView.Adapter<ItemViewHolder>() {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-            val binding =
-                ItemCardViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            return ItemViewHolder(binding)
-        }
-
-        override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-            val currentItem = items[position]
-
-            holder.bind(currentItem)
-
-            holder.itemView.setOnClickListener {
-                Toast.makeText(
-                    holder.itemView.context,
-                    "Clic en: ${currentItem.id}",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-
-        override fun getItemCount(): Int = items.size
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val binding =
+            ItemCardViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ItemViewHolder(binding)
     }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val currentItem = items[position]
+
+        holder.bind(currentItem)
+
+        holder.itemView.setOnClickListener {
+            // Obtener el NavController
+            val navController = Navigation.findNavController(holder.itemView)
+
+            // Crear un bundle con el ID del usuario
+            val bundle = Bundle().apply {
+                putInt("userId", currentItem.id)
+            }
+
+            // Navegar al FragmentMatchProfile pasando el ID
+            navController.navigate(R.id.match_profile_fragment, bundle)
+        }
+    }
+
+    override fun getItemCount(): Int = items.size
+}
