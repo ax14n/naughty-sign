89164fd79c57c872b7eb63e373b1ebae35465de2
me <zelmarhernanramilopiazzoli2202@gmail.com>
2025-01-21 19:16:32 +0100

    Fix: not working animation

--- app/src/main/java/com/example/naughty_sign/fragments/FragmentProfileSwipe.kt
+++ app/src/main/java/com/example/naughty_sign/fragments/FragmentProfileSwipe.kt
@@ -1,18 +1,15 @@
 package com.example.naughty_sign.fragments
 
-import android.graphics.drawable.AnimationDrawable
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.animation.doOnEnd
 import androidx.fragment.app.Fragment
 import com.example.naughty_sign.databinding.FragmentProfileSwipeBinding
 import com.example.naughty_sign.utils.LoggedUserUtils
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
 
 /**
  * A simple [Fragment] subclass.
@@ -30,31 +27,58 @@ class FragmentProfileSwipe : Fragment() {
 
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentProfileSwipeBinding.inflate(layoutInflater)
 
         // ------- { Asignación de boton de Like! } ------- //
-        val animation_corason = binding.corason.background as AnimationDrawable
+
         binding.btnLike.setOnClickListener {
             LoggedUserUtils.almacenarLike("")
             cambiarUsuario()
-            binding.corason.visibility= View.VISIBLE
-            animation_corason.start()
-            CoroutineScope(Dispatchers.Main).launch {
-                Thread.sleep(1_000)  // wait for 1 second
-                binding.corason.visibility= View.INVISIBLE
+            val scaleX = ObjectAnimator.ofFloat(binding.corason, "scaleX", 1f, 0.5f)
+            val scaleY = ObjectAnimator.ofFloat(binding.corason, "scaleY", 1f, 0.5f)
+
+            binding.corason.visibility = View.VISIBLE
+
+            scaleX.repeatCount = 3
+            scaleY.repeatCount = 3
+            val animatorSet = AnimatorSet().also {
+                it.play(scaleX).with(scaleY)
+                it.start()
             }
+            animatorSet.doOnEnd {
+                binding.corason.visibility = View.GONE
+            }
+
         }
 
-
-
+//        val animation_corason = binding.corason. as AnimationDrawable
+//            binding.corason.visibility = View.VISIBLE
+//            animation_corason.start() // Se inicia
+////            CoroutineScope(Dispaxtchers.Main).launch {
+//            Log.d("HOLA", "Se ha lanzado el hilo")
+//            binding.corason.visibility = View.INVISIBLE
+//            Thread.sleep(1_000)  // wait for 1 second
+////            }
         // ------- { Asignación de boton de Next! } ------- //
         binding.btnDislike.setOnClickListener {
-            // TODO: Nos vamos a inmolar porque vamos a tener que montar un servidor.
             cambiarUsuario()
+            val scaleX = ObjectAnimator.ofFloat(binding.corason, "scaleX", 1f, 0.5f)
+            val scaleY = ObjectAnimator.ofFloat(binding.corason, "scaleY", 1f, 0.5f)
+
+            binding.corason.visibility = View.VISIBLE
+
+            scaleX.repeatCount = 3
+            scaleY.repeatCount = 3
+            val animatorSet = AnimatorSet().also {
+                it.play(scaleX).with(scaleY)
+                it.start()
+            }
+            animatorSet.doOnEnd {
+                binding.corason.visibility = View.GONE
+            }
         }
 
         // ------- { Puesta en escena de la vista } ------- //
