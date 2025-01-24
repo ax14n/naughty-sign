6a13198827c76658fae29205cc5c1fcc1571e97d
ax14n <zelmarhernanramilopiazzoli2202@gmail.com>
2025-01-24 12:39:49 +0100

    Feat: Guide now works correctly and blocks user interaction with app

--- app/src/main/java/com/example/naughty_sign/activities/MainActivity.kt
+++ app/src/main/java/com/example/naughty_sign/activities/MainActivity.kt
@@ -4,6 +4,7 @@ import NotificationHelper
 import android.Manifest
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.media.SoundPool
@@ -29,7 +30,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var guideBinding: GuideBinding
 
     private var needToStartGuide: Boolean = true
-    private var stepGuide: Int = 0;
 
     /**
      * Se inicializan variables y se preparan recursos no relacionados con la vista.
@@ -92,37 +92,85 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Inicia la guía.
+     */
+    @SuppressLint("Recycle")
     private fun initializeGuide() {
+        // --- { ABANDONO DE LA GUÍA. } --- //
         guideBinding.exitGuide.setOnClickListener(::onExitGuide)
 
+        // --- { COMIENZO DE LA GUÍA. } --- //
         if (needToStartGuide) {
 
+            // --- { HACE LA GUÍA VISIBLE } --- //
             guideBinding.guideLayout.visibility = View.VISIBLE
+            guideBinding.textStep.visibility = View.VISIBLE
 
+            // --- { ANIMACIONES CORREGIDAS } --- //
+            val fadeTextStep = ObjectAnimator.ofFloat(guideBinding.textStep, "alpha", 0f, 1f)
+            fadeTextStep.repeatCount = 3                        // Veces que se repetirá
+            fadeTextStep.repeatMode = ObjectAnimator.REVERSE    // Efecto "parpadeo"
 
-            val scaleX = ObjectAnimator.ofFloat(guideBinding.pulseImage, "scaleX", 1f, 0.5f)
-            val scaleY = ObjectAnimator.ofFloat(guideBinding.pulseImage, "scaleY", 1f, 0.5f)
+            val fadePulseImage = ObjectAnimator.ofFloat(guideBinding.pulseImage, "alpha", 0f, 0.8f)
+            fadePulseImage.repeatCount = 3                      // Veces que se repetirá
+            fadePulseImage.repeatMode = ObjectAnimator.REVERSE  // Efecto "parpadeo"
 
-            val fadeIn = ObjectAnimator.ofFloat(guideBinding.textStep, "alpha", 0f, 1f)
-
-            scaleX.repeatCount = 3
-            scaleY.repeatCount = 3
+            // --- { INICIADOR DE LAS ANIMACIONES } --- //
             val animatorSet = AnimatorSet()
-            animatorSet.play(scaleX).with(scaleY).before(fadeIn)
 
+            // --- { INICIA LAS ANIMACIONES SIN CONFLICTOS } --- //
+            animatorSet.playTogether(fadeTextStep, fadePulseImage)
+            animatorSet.duration = 1000                         // Duración de las animaciones
+            animatorSet.start()                                 // Se inician las animaciones
 
-            animatorSet.duration = 1000 // Duración de la animación
-            animatorSet.start()
+            // --- { SEGUNDA SECUENCIA DE INSTRUCCIONES } --- //
             animatorSet.doOnEnd {
+
+                // --- { OCULTACIÓN DE ELEMENTOS DEL MENÚ ANTERIOR } --- //
+                guideBinding.pulseImage.visibility = View.GONE
+                guideBinding.textStep.visibility = View.GONE
+
                 if (needToStartGuide) {
-                    guideBinding.pulseImage.visibility = View.GONE
-                    guideBinding.textStep.visibility = View.VISIBLE
+
+                    guideBinding.textStep2.visibility = View.VISIBLE
+                    guideBinding.buttonsGuide.visibility = View.VISIBLE
+
+                    // --- { ANIMACIONES CORREGIDAS } --- //
+                    val fadeTextStep2 =
+                        ObjectAnimator.ofFloat(guideBinding.textStep2, "alpha", 0f, 1f)
+                    fadeTextStep2.repeatCount = 3                        // Veces que se repetirá
+                    fadeTextStep2.repeatMode = ObjectAnimator.REVERSE    // Efecto "parpadeo"
+
+                    val fadePulseImage2 =
+                        ObjectAnimator.ofFloat(guideBinding.buttonsGuide, "alpha", 0f, 0.8f)
+                    fadePulseImage2.repeatCount = 3                      // Veces que se repetirá
+                    fadePulseImage2.repeatMode = ObjectAnimator.REVERSE  // Efecto "parpadeo"
+
+                    // --- { INICIADOR DE LAS ANIMACIONES } --- //
+                    val animatorSet2 = AnimatorSet()
+
+                    // --- { INICIA LAS ANIMACIONES SIN CONFLICTOS } --- //
+                    animatorSet2.playTogether(fadeTextStep2, fadePulseImage2)
+                    animatorSet2.duration =
+                        1000                         // Duración de las animaciones
+                    animatorSet2.start()             // Se inician las animaciones
+
+                    animatorSet2.doOnEnd {
+                        guideBinding.buttonsGuide.visibility = View.GONE
+                        guideBinding.textStep2.visibility = View.GONE
+                        guideBinding.guideLayout.visibility = View.GONE
+                    }
                 }
             }
+
 
         }
     }
 
+    /**
+     * Abandona la guía.
+     */
     private fun onExitGuide(view: View?) {
         needToStartGuide = false;
 
